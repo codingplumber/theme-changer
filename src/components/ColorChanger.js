ef8df17e205ab5e520c3437a59b9b305871fce64
codingplumber <jason.stickel@live.com>
2017-05-17 15:51:49 -0600

    finished project

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 
 export default class ColorChanger extends Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       allowEdit: this.props.allowEdit
     };
   }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps(props) {
+    this.setState({ allowEdit: props.allowEdit });
+  }
+  //this method takes in a props parameter and uses setState to update the value of allowEdit on state to the value of allowEdit on props
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => { this.props.update(e.target.value) } }  disabled={ this.state.allowEdit === 'false' }>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 
 export default class SizeChanger extends Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       allowEdit: this.props.allowEdit
     };
   }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps(props) {
+    this.setState({ allowEdit: props.allowEdit });
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => this.props.update( parseInt(e.target.value) ) } disabled={ this.state.allowEdit === 'false' }>
+      { /* disabled prop disables the select element (e) if allowEdit on state is equal to false */ }
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
